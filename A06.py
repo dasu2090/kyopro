4739ee5a6a9fe12ecc1488c6160d02b5c17ef480
sia-6 <myemail@example.com>
2024-09-11 21:40:29 +0900

    fix A06.py

--- A06.py
+++ A06.py
@@ -1,7 +1,10 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
 
+S = [0] * (N + 1)
 for i in range(N):
-    pass
-for i in range(Q):
-    pass
+    S[i+1] = S[i] + A[i]
+
+for _ in range(Q):
+    L, R = map(int, input().split())
+    print(S[R] - S[L - 1])
