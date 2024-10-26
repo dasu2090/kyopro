8a7ad2734fe7cbe470cc3ac357702fc0a5e8e601
sia-6 <myemail@example.com>
2024-10-26 22:05:00 +0900

    Linear Search

--- C1/A02.py
+++ C1/A02.py
@@ -1,7 +1,12 @@
 n, x = map(int, input().split())
-def f(a):
-    for i in range(n):
-        if x == a[i]:
-            return 'Yes'
-    return 'No'
-print(f(list(map(int, input().split()))))
+a = list(map(int, input().split()))
+print("Yes") if any(i == x for i in a) else print("No")
+
+#-----------------
+# n, x = map(int, input().split())
+# def f(a):
+#     for i in range(n):
+#         if x == a[i]:
+#             return 'Yes'
+#     return 'No'
+# print(f(list(map(int, input().split()))))
--- C1/A04.py
+++ C1/A04.py
@@ -4,6 +4,7 @@ for i in range(9,-1, -1):
     print((n // d) % 2, end='')
 print()
 
+#-------------------
 # n = int(input())
 
 # for i in range(9, -1, -1):
