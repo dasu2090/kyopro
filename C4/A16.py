d7634e82415e01f5c687511763305edb514378b6
sia-6 <myemail@example.com>
2024-10-27 22:19:47 +0900

    動的計画法

---
+++ C4/A16.py
@@ -0,0 +1,30 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+dp[2] = a[0]
+
+for i in range(3, n + 1):
+    dp[i] = min(dp[i - 1] + a[i - 2], dp[i - 2] + b[i - 3])
+
+print(dp[n])
+
+#----------------------------
+# n = int(input())
+# at = list(map(int, input().split()))
+# bt = list(map(int, input().split()))
+# a = {}
+# b = {}
+# for i in range(2, n + 1):
+#     a[i] = at[i - 2]
+# for i in range(3, n + 1):
+#     b[i] = bt[i - 3]
+
+# dp = [0] * (n + 1)
+# dp[1] = 0
+# dp[2] = a[2]
+# for i in range(3, n + 1):
+#     dp[i] = min(dp[i - 1] + a[i], dp[i - 2] + b[i])
+
+# print(dp[n])
