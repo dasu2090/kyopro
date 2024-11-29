5fe845a3e653ff114fd2cb66f35952d99afec498
sia-6 <myemail@example.com>
2024-11-29 23:10:59 +0900

    動的計画法の復元

--- C4/A17.py
+++ C4/A17.py
@@ -7,22 +7,43 @@ dp[2] = a[0]
 for i in range(3, n + 1):
     dp[i] = min(dp[i - 1] + a[i - 2], dp[i - 2] + b[i - 3])
 
-p = n
 ans = []
-def f(ans, p):
+p = n
+print(dp)
+while p > 0:
     ans.append(p)
-    if p == 1:
-        return
-    if dp[p - 1] + a[p - 2] == dp[p]:
-        return f(ans, p - 1)
+    if dp[p-2] + b[p-3] == dp[p]:
+        p = p - 2
     else:
-        return f(ans, p - 2)
+        p = p - 1
 
-f(ans, p)
-print(len(ans))
-for i in range(len(ans) - 1, -1, -1):
-    print(ans[i], end=' ')
-print()
+print(ans)
+#--------------------------
+# n = int(input())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+
+# dp = [0] * (n + 1)
+# dp[2] = a[0]
+# for i in range(3, n + 1):
+#     dp[i] = min(dp[i - 1] + a[i - 2], dp[i - 2] + b[i - 3])
+
+# p = n
+# ans = []
+# def f(ans, p):
+#     ans.append(p)
+#     if p == 1:
+#         return
+#     if dp[p - 1] + a[p - 2] == dp[p]:
+#         return f(ans, p - 1)
+#     else:
+#         return f(ans, p - 2)
+
+# f(ans, p)
+# print(len(ans))
+# for i in range(len(ans) - 1, -1, -1):
+#     print(ans[i], end=' ')
+# print()
 #------------------
 # n = int(input())
 # a = list(map(int, input().split()))
