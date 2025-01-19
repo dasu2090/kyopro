f0076c47d315d59f025586ed0e5581a4dcdd5df9
sia-6 <myemail@example.com>
2025-01-19 19:29:23 +0900

    how many quests?

--- C2/A06.py
+++ C2/A06.py
@@ -1,14 +1,28 @@
 n, q = map(int, input().split())
 a = list(map(int, input().split()))
 
-for i in range(1, n):
-    a[i] += a[i - 1]
+b = [0] * (n+1)
+b[1] = a[0]
+for i in range(2, n+1):
+    b[i] = a[i-1] + b[i-1]
+
 for _ in range(q):
     l, r = map(int, input().split())
-    if l != 1:
-        print(a[r - 1] - a[l - 2])
-    else:
-        print(a[r - 1])
+    print(b[r]-b[l-1])
+print(b)
+
+
+# n, q = map(int, input().split())
+# a = list(map(int, input().split()))
+
+# for i in range(1, n):
+#     a[i] += a[i - 1]
+# for _ in range(q):
+#     l, r = map(int, input().split())
+#     if l != 1:
+#         print(a[r - 1] - a[l - 2])
+#     else:
+#         print(a[r - 1])
 
 
 # N, Q = map(int, input().split())
