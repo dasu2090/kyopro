7a9aef19231815b6547a6b84a8cc0826c6267ec8
sia-6 <myemail@example.com>
2025-01-23 22:30:29 +0900

    Dungeon 1

--- C4/A16.py
+++ C4/A16.py
@@ -2,11 +2,22 @@ n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-dp = [0] * (n + 1)
+dp = [0] * (n+1)
 dp[2] = a[0]
 for i in range(3, n+1):
     dp[i] = min(dp[i-1] + a[i-2], dp[i-2] + b[i-3])
 print(dp[n])
+
+#------------------------
+# n = int(input())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+
+# dp = [0] * (n + 1)
+# dp[2] = a[0]
+# for i in range(3, n+1):
+#     dp[i] = min(dp[i-1] + a[i-2], dp[i-2] + b[i-3])
+# print(dp[n])
 
 #------------------------
 # n = int(input())
