480f16aca8d80a4f09c9e74ebee02e9310294eb8
sia-6 <myemail@example.com>
2025-03-01 23:47:18 +0900

    compression

--- C3/A15.py
+++ C3/A15.py
@@ -3,12 +3,25 @@ import bisect
 n = int(input())
 a = list(map(int, input().split()))
 
-s = list(set(a))
-s.sort()
-b = [0] * (n * 1)
-for i in range(n):
-    b[i] = bisect.bisect_right(s,a[i])
+tmp = a
+b = list(set(a))
+b.sort()
 
-for i in b:
-    print(i, end=" ")
+for i in range(n):
+    print(bisect.bisect_right(b,tmp[i]), end=" ")
 print()
+
+# import bisect
+
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# s = list(set(a))
+# s.sort()
+# b = [0] * (n * 1)
+# for i in range(n):
+#     b[i] = bisect.bisect_right(s,a[i])
+
+# for i in b:
+#     print(i, end=" ")
+# print()
